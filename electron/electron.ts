c14782863a6ba4f2122a751e38647be9a471f557
junseong <junseongday@gmail.com>
2023-07-07 17:12:45 +0900

    chore: 앱 크기 조정

--- electron/electron.ts
+++ electron/electron.ts
@@ -7,7 +7,7 @@ function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 800,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, "preload.ts"),
       nodeIntegration: true,
